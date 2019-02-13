7b7c249e9076114bf7b5133774ff651210eb876a
CurbYourStrangeness <36433882+CurbYourStrangeness@users.noreply.github.com>
2019-02-13 11:06:51 -0600

    Create game0.rb
    Sample intro to "Get my number" game

---
+++ game0.rb
@@ -0,0 +1,12 @@
+#Get my number game (redundant because fun
+!)
+#Written by "Jack Miller"
+
+puts "Welcome to Eating Unripe Things for Beginners"
+puts "Want my number?"
+
+print "What's your name this time?"
+
+input = gets
+
+puts "Welcome #{input]!"
